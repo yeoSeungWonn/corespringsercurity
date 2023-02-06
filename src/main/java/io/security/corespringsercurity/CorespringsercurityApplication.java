db6fc336d25685d1f40fcc85333b26f379f68464
dutmddnjs <dutmddnjs@hanmail.net>
2023-02-06 17:09:17 +0900

    Initial commit

---
+++ src/main/java/io/security/corespringsercurity/CorespringsercurityApplication.java
@@ -0,0 +1,13 @@
+package io.security.corespringsercurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorespringsercurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorespringsercurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/security/corespringsercurity/CorespringsercurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.corespringsercurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorespringsercurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
